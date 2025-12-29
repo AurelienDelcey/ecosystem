9af617405d856b7ce19770f16cc4a63418dd27ba
AurelienDelcey <aurelien.delcey@gmail.com>
2025-12-30 00:17:55 +0100

    add Herbivore class with populationDelta accounting for predator presure

---
+++ src/main/java/model/herbivore.java
@@ -0,0 +1,20 @@
+package model;
+
+public class herbivore extends Animal{
+
+	public herbivore(String species, double reproductionRate, double mortalityRate, int initialPopulation) {
+		super(species, reproductionRate, mortalityRate, initialPopulation);
+	}
+
+	@Override
+	double populationDelta(int predatorPopulation) {
+	    double delta = super.getCurrentPopulation() * (super.getReproductionRate() - super.getMortalityRate());
+	    double predatorFactor = 1.0;
+	    if (predatorPopulation > 0) {
+	        predatorFactor = Math.min(1.0, (double) predatorPopulation / 
+	        		(predatorPopulation + super.getCurrentPopulation()));
+	    }
+
+	    return delta * (1 - predatorFactor);
+	}
+}
